b728fca7a5f21f56001d23f9c2e4711bbb4c8421
leah_moore02@icloud.com <leah_moore02@icloud.com>
2022-01-25 15:19:26 +0000

    second commit

---
+++ lab2-8.py
@@ -0,0 +1,10 @@
+places = ["Italy", "Germany", "Greece", "Australia", "Croatia"]
+
+print(places[0])
+print(places[1])
+print(places[2])
+print(places[3])
+print(places[4])
+print(sorted(places))
+
+places.sort(reverse=True)
---
+++ lab2.py
@@ -0,0 +1,6 @@
+names =["Jack", "Cian", "Elliot", "Cai", "Eben"]
+print("Hello " + names[0])
+print("Hello " + names[1])
+print("Hello " + names[2])
+print("Hello " + names[3])
+print("Hello " + names[4])
